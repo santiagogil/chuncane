6fca65d1e106197d7911f521ebfe1c00c6473712
Santiago Gil <gil.educaciontdf@gmail.com>
2019-08-01 15:20:11 -0300

    Optimiza js

--- src/js/app.js
+++ src/js/app.js
@@ -4,43 +4,13 @@ var lozad = require('lozad')
 const observer = lozad()
 
 window.addEventListener('load', () => {
-  observer.observe()
+  requestIdleCallback(observer.observe)
+  requestIdleCallback(sharebutton)
+  requestIdleCallback(sidebar)
   
-  let sidebar = document.getElementById('sidebar')
-  let hamburger = document.getElementById('hamburger')
-  let close = document.getElementById('close')
-  hamburger.addEventListener('click', event => {
-      event.preventDefault()
-      sidebar.classList.toggle('transform-off')
-  })
-  close.addEventListener('click', event => {
-      event.preventDefault()
-      hamburger.style.transform = ''
-      sidebar.classList.toggle('transform-off')
-  })
 
-  let url = window.location.href
-  let title = document.title
-  let shareul = document.getElementById('share')
-  if (shareul && navigator.share) {
-  console.log(shareul && window.navigator.share != false)
-    let shareicon = document.createElement('li')
-    let inner = `<span id="web-share" href="" class="link bg-primary white db relative br-100 pa2" aria-label="Compartir">
-    <svg width="16px" height="16px" class="db">
-      <use xlink:href="#icons-share"></use>
-    </svg>
-  </span>`
-    shareicon.innerHTML = inner
-    console.log(inner)
-    shareicon.classList.add('dib', 'ph2', 'raise')
-    shareul.appendChild(shareicon)
-    console.log(shareul)
-    shareicon.addEventListener('click', event => {
-      navigator.share({"url": url, "title": title})
-      .then(console.log)
-      .catch(console.log)
-    })
-  }
+
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
@@ -63,3 +33,43 @@ window.addEventListener('load', () => {
 // }
 // Check that service workers are registered
 
+function sharebutton () {
+  let url = window.location.href
+  let title = document.title
+  let shareul = document.getElementById('share')
+  if (shareul && navigator.share) {
+    let shareicon = document.createElement('li')
+    let inner = `<span id="web-share" href="" class="link bg-primary white db relative br-100 pa2" aria-label="Compartir">
+    <svg width="16px" height="16px" class="db">
+      <use xlink:href="#icons-share"></use>
+    </svg>
+  </span>`
+    shareicon.innerHTML = inner
+    console.log(inner)
+    shareicon.classList.add('dib', 'ph2', 'raise')
+    shareul.appendChild(shareicon)
+    console.log(shareul)
+    shareicon.addEventListener('click', event => {
+      navigator.share({"url": url, "title": title})
+      .then(console.log)
+      .catch(console.log)
+    })
+  }
+}
+function sidebar () {
+  let sidebar = document.getElementById('sidebar')
+  let hamburger = document.getElementById('hamburger')
+  let close = document.getElementById('close')
+  hamburger.addEventListener('click', event => {
+      event.preventDefault()
+      requestAnimationFrame(transformoff)
+  })
+  close.addEventListener('click', event => {
+      event.preventDefault()
+      hamburger.style.transform = ''
+      requestAnimationFrame(transformoff)
+  })
+function transformoff () {
+  sidebar.classList.toggle('transform-off')
+}
+}
